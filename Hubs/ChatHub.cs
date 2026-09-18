3ec8db2b04f0b353d88656042f414d94c1c86f0c
Brian Njuguna <briannjuguna694@gmail.com>
2026-09-18 11:42:34 +0300

    first commit

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalIRChat.Hubs
+{
+    public class ChatHub: Hub
+    {
+        //add user connection to a spesific group
+        public async Task JoinRoom(string roomName, string userName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+            await Clients.Group(roomName).SendAsync("ReceiveSystemMessage", $"{userName} has joined the room.");
+        }
+
+        //remove user connection from a spesific group
+        public async Task LeaveRoom(string roomName, string userName) {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
+            await Clients.Group(roomName).SendAsync("ReceiveSystemMessage", $"User {userName} has left the room");
+        }
+
+        //send message only to clients inside a speific room
+        public async Task SendMessage(string roomName, string userName, string message) 
+        {
+            await Clients.Group(roomName).SendAsync("ReceiveGroupMessage", userName, message);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+using SignalIRChat.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//adding signalIR
+builder.Services.AddSignalR();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+//map SignalIR hub endpoin
+app.MapHub<ChatHub>("/chatHub");
+app.Run();
